8ed9a9f8ed3ce7a56f66bfdbaa7f836004c18af2
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-10-11 01:20:32 +0300

    Add styles for hamburger menu and some general css

--- client/src/components/MainNavigation/MainNavigation.js
+++ client/src/components/MainNavigation/MainNavigation.js
@@ -1,16 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css'
+
 const MainNavigation = () => {
     return (
-        <header>
-            <div className="hamburger-menu">
-                <input type="checkbox" />
-            </div>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/movies">Movies</NavLink></li>
-                <li><NavLink to="/series">TV Shows</NavLink></li>
-            </ul>
+        <header className={classes.header}>
+            <nav className={classes.nav}>
+                <div className={classes.toggle}>
+                    <div className={classes.hamburger}>
+                        <input type="checkbox" />
+                        <div className={classes['hamburger-top']}></div>
+                        <div className={classes['hamburger-middle']}></div>
+                        <div className={classes['hamburger-bottom']}></div>
+                    </div>
+                </div>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/movies">Movies</NavLink></li>
+                    <li><NavLink to="/series">TV Shows</NavLink></li>
+                </ul>
+            </nav>
         </header>
     );
 };
