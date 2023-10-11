e3d1f8897c4a09933527cef78c71a4e25a88b81d
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-10-11 12:46:39 +0300

    Styling the navbar for small screens

--- client/src/components/MainNavigation/MainNavigation.js
+++ client/src/components/MainNavigation/MainNavigation.js
@@ -6,18 +6,34 @@ const MainNavigation = () => {
     return (
         <header className={classes.header}>
             <nav className={classes.nav}>
-                <div className={classes.toggle}>
-                    <div className={classes.hamburger}>
-                        <input type="checkbox" />
-                        <div className={classes['hamburger-top']}></div>
-                        <div className={classes['hamburger-middle']}></div>
-                        <div className={classes['hamburger-bottom']}></div>
-                    </div>
+                <input type="checkbox" />
+                <div className={classes.hamburger}>
+                    <div className={classes['hamburger-top']}></div>
+                    <div className={classes['hamburger-middle']}></div>
+                    <div className={classes['hamburger-bottom']}></div>
                 </div>
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/movies">Movies</NavLink></li>
-                    <li><NavLink to="/series">TV Shows</NavLink></li>
+                <ul className={classes.links}>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
+                            to="/"
+                        >Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
+                            to="/movies"
+                        >Movies
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
+                            to="/series"
+                        >TV Shows
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
