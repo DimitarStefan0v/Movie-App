b020c4a5987a46a5f96588be94a7de4113550862
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-10-11 14:11:10 +0300

    Remake toggle navigation instead with input checkbox with useState

--- client/src/components/MainNavigation/MainNavigation.js
+++ client/src/components/MainNavigation/MainNavigation.js
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css'
 
 const MainNavigation = () => {
+    const [showNavigation, setShowNavigation] = useState(false);
+
+    const toggleNavigation = () => {
+        setShowNavigation(state => !state);
+    };
+
     return (
         <header className={classes.header}>
             <nav className={classes.nav}>
-                <input type="checkbox" />
-                <div className={classes.hamburger}>
+                <div className={showNavigation ? `${classes.hamburger} ${classes.open}` : `${classes.hamburger}`} onClick={toggleNavigation}>
                     <div className={classes['hamburger-top']}></div>
                     <div className={classes['hamburger-middle']}></div>
                     <div className={classes['hamburger-bottom']}></div>
                 </div>
-                <ul className={classes.links}>
+                <ul className={showNavigation ? `${classes.links}` : `${classes.links} ${classes.collapse}`}>
                     <li>
                         <NavLink
                             className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
                             to="/"
+                            onClick={toggleNavigation}
                         >Home
                         </NavLink>
                     </li>
@@ -24,6 +31,7 @@ const MainNavigation = () => {
                         <NavLink
                             className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
                             to="/movies"
+                            onClick={toggleNavigation}
                         >Movies
                         </NavLink>
                     </li>
@@ -31,6 +39,7 @@ const MainNavigation = () => {
                         <NavLink
                             className={({ isActive }) => isActive ? `${classes.link} ${classes.active}` : `${classes.link}`}
                             to="/series"
+                            onClick={toggleNavigation}
                         >TV Shows
                         </NavLink>
                     </li>
