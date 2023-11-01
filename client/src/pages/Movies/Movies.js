b1a62a5ea88018c0c6f64f6de61bf66efa15b5de
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-01 12:11:51 +0200

    Use the getMovies service to get the movies in Movies Page

--- client/src/pages/Movies/Movies.js
+++ client/src/pages/Movies/Movies.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+import { getMoviesWithPages } from '../../services/getMovies';
+
 const MoviesPage = () => {
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const movies = await getMoviesWithPages(1);
+            console.log(movies);
+        }
+
+        fetchMovies();
+    },
+        []);
+
     return (
         <div>Movies Page</div>
     );
