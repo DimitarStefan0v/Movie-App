b7f2a5a3f14e8a5d14104fe110e2ca1b270a9301
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-02 00:38:17 +0200

    Add title under the movie poster

--- client/src/components/UI/Card/Card.js
+++ client/src/components/UI/Card/Card.js
@@ -4,7 +4,10 @@ import classes from './Card.module.css';
 
 const Card = (props) => {
     return (
-        <div className={classes.card}><img src={urls.images + props.movie.poster_path} alt={props.movie.title} /></div>
+        <div className={classes.card}>
+            <img src={urls.images + props.movie.poster_path} alt={props.movie.title} />
+            <div className={classes.title}>{props.movie.title}</div>
+        </div>
     );
 };
 
