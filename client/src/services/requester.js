72de0ec324367efbb0752e422d3d1ab8244ea6c0
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-01 12:10:21 +0200

    Add util function for requesting data (only for get requests as i'm using a public api)

---
+++ client/src/services/requester.js
@@ -0,0 +1,20 @@
+// the requester is only for get requests as I'm using a public API 
+
+export const requester = async (url) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TOKEN}`
+        }
+    };
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        console.log(response);
+    }
+
+    const data = await response.json();
+    return data;
+};
