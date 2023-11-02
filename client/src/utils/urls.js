3d58834881ea93e07c4f5d505db56187213f0553
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-02 23:52:48 +0200

    Add defaultImage for movies and series without poster path

--- client/src/utils/urls.js
+++ client/src/utils/urls.js
@@ -1,4 +1,6 @@
 export const urls = {
     images: 'https://image.tmdb.org/t/p/w500',
+    defaultImage: 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
     movies: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US',
+    series: 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US',
 };
