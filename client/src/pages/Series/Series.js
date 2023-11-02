bd0d65cf63568488df6558902045166d4a6a3841
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-02 23:49:58 +0200

    Add logic to series component

--- client/src/pages/Series/Series.js
+++ client/src/pages/Series/Series.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getSeriesWithPages } from '../../services/getSeries';
+
+import CardList from '../../components/UI/CardList/CardList';
+import Card from '../../components/UI/Card/Card';
+import Pagination from '../../components/UI/Pagination/Pagination';
+
 const SeriesPage = () => {
-  return (
-    <div>Series Page</div>
-  );
+    const [seriesData, setSeriesData] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchSeries = async () => {
+            const series = await getSeriesWithPages(currentPage);
+            setSeriesData(series);
+            window.scrollTo(0, 0);
+        };
+
+        fetchSeries().catch(err => navigate('/error'));
+    },
+        [currentPage, navigate]);
+
+    const changePageHandler = (selectedPage) => {
+        setCurrentPage(selectedPage);
+    };
+
+    return (
+        <>
+            <CardList>{seriesData.results?.map(serie => <Card key={serie.id} poster={serie.poster_path} title={serie.name} />)}</CardList>
+            <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={500} />
+        </>
+    );
 };
 
 export default SeriesPage;
