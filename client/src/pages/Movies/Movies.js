f179d836cd2801c368f63df6bff704eeeaec8d1f
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-02 12:04:50 +0200

    Add ChangePageHandler in Movies Component

--- client/src/pages/Movies/Movies.js
+++ client/src/pages/Movies/Movies.js
@@ -3,23 +3,35 @@ import { getMoviesWithPages } from '../../services/getMovies';
 
 import CardList from '../../components/UI/CardList/CardList';
 import Card from '../../components/UI/Card/Card';
+import Pagination from '../../components/UI/Pagination/Pagination';
+
 
 const MoviesPage = () => {
     const [moviesData, setMoviesData] = useState({});
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const movies = await getMoviesWithPages(1);
+            const movies = await getMoviesWithPages(currentPage);
             setMoviesData(movies);
+            window.scrollTo(0, 0);
         }
 
         fetchMovies();
     },
-        []);
+        [currentPage]);
+
+    const changePageHandler = (selectedPage) => {
+        setCurrentPage(selectedPage);
+    };
 
     return (
-        <CardList>{moviesData.results?.map(movie => <Card key={movie.id} movie={movie} />)}</CardList>
-        );
+        <>
+            <CardList>{moviesData.results?.map(movie => <Card key={movie.id} movie={movie} />)}
+            </CardList>
+            <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={500} />
+        </>
+    );
 };
 
 export default MoviesPage;
--- client/src/services/getMovies.js
+++ client/src/services/getMovies.js
@@ -1,7 +1,7 @@
 import { requester } from './requester';
 import { urls } from '../utils/urls';
 
-export const getMoviesWithPages = (page = 1) => {
-    const movies = requester(urls.movies + `&page=${page}&sort_by=primary_release_date.asc`);
+export const getMoviesWithPages = (page = 1, sort = 'popularity.desc') => {
+    const movies = requester(urls.movies + `&page=${page}&sort_by=${sort}`);
     return movies;
 };
