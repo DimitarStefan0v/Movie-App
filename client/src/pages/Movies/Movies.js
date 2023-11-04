882550fd1ee9d3e4bdf79ac77f322216bf19fc62
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-04 18:04:23 +0200

    Render the sorter component when showing movies and series

--- client/src/pages/Movies/Movies.js
+++ client/src/pages/Movies/Movies.js
@@ -5,33 +5,41 @@ import { getMoviesWithPages } from '../../services/getMovies';
 
 import CardList from '../../components/UI/CardList/CardList';
 import Card from '../../components/UI/Card/Card';
+import Sorter from '../../components/UI/Sorter/Sorter';
 import Pagination from '../../components/UI/Pagination/Pagination';
 
 
 const MoviesPage = () => {
     const [moviesData, setMoviesData] = useState({});
     const [currentPage, setCurrentPage] = useState(1);
+    const [currentFilter, setCurrentFilter] = useState('popularity.desc');
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const movies = await getMoviesWithPages(currentPage);
+            const movies = await getMoviesWithPages(currentPage, currentFilter);
             setMoviesData(movies);
             window.scrollTo(0, 0);
         }
 
         fetchMovies().catch(err => navigate('/error'));
     },
-        [currentPage, navigate]);
+        [currentPage, currentFilter, navigate]);
 
     const changePageHandler = (selectedPage) => {
         setCurrentPage(selectedPage);
     };
 
+    const changeSortQueryHandler = (selectedQuery) => {
+        setCurrentFilter(selectedQuery);
+    };
+
+
+
     return (
         <>
-            <CardList>{moviesData.results?.map(movie => <Card key={movie.id} poster={movie.poster_path} title={movie.title} />)}
-            </CardList>
+            <Sorter currentFilter={currentFilter} changeSortQueryHandler={changeSortQueryHandler} />
+            <CardList>{moviesData.results?.map(movie => <Card key={movie.id} poster={movie.poster_path} title={movie.title} />)}</CardList>
             <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={500} />
         </>
     );
--- client/src/pages/Series/Series.js
+++ client/src/pages/Series/Series.js
@@ -5,30 +5,37 @@ import { getSeriesWithPages } from '../../services/getSeries';
 
 import CardList from '../../components/UI/CardList/CardList';
 import Card from '../../components/UI/Card/Card';
+import Sorter from '../../components/UI/Sorter/Sorter';
 import Pagination from '../../components/UI/Pagination/Pagination';
 
 const SeriesPage = () => {
     const [seriesData, setSeriesData] = useState({});
     const [currentPage, setCurrentPage] = useState(1);
+    const [currentFilter, setCurrentFilter] = useState('popularity.desc');
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchSeries = async () => {
-            const series = await getSeriesWithPages(currentPage);
+            const series = await getSeriesWithPages(currentPage, currentFilter);
             setSeriesData(series);
             window.scrollTo(0, 0);
         };
 
         fetchSeries().catch(err => navigate('/error'));
     },
-        [currentPage, navigate]);
+        [currentPage, currentFilter, navigate]);
 
     const changePageHandler = (selectedPage) => {
         setCurrentPage(selectedPage);
     };
 
+    const changeSortQueryHandler = (selectedQuery) => {
+        setCurrentFilter(selectedQuery);
+    };
+
     return (
         <>
+            <Sorter currentFilter={currentFilter} changeSortQueryHandler={changeSortQueryHandler} />
             <CardList>{seriesData.results?.map(serie => <Card key={serie.id} poster={serie.poster_path} title={serie.name} />)}</CardList>
             <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={500} />
         </>
