f15d56f3b5ed921fa48113ab9e1f5e1afcb94dcc
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-21 10:33:17 +0200

    Add Details page for movie and styles for it

--- client/src/pages/Movies/MovieDetails.js
+++ client/src/pages/Movies/MovieDetails.js
@@ -1,24 +1,61 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { getMovieById } from '../../services/moviesService';
+import { urls } from '../../utils/urls';
+
+import Loader from '../../components/UI/Loader/Loader';
+
+import classes from './Movies.module.css';
 
 const MovieDetailsPage = () => {
     const { movieId } = useParams();
-    const [movie, setMovie] = useState();
+    const [movie, setMovie] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const getMovie = async () => {
+        const fetchMovie = async () => {
             const movie = await getMovieById(movieId);
             setMovie(movie);
         };
 
-        getMovie()
-    }, [movieId]);
+        setIsLoading(true);
+
+        const timer = setTimeout(async () => {
+            try {
+                await fetchMovie();
+            } catch (error) {
+                navigate('/error');
+            }
+
+            setIsLoading(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+
+
+    }, [movieId, navigate]);
+
+    const showMovie = <div className={classes.wrapper}> {console.log(movie)}
+        <h2>{movie.title}</h2>
+        <div className={classes['image-overview']}>
+            <div className={classes['image-wrapper']}><img src={movie.poster_path ? urls.images + movie.poster_path : urls.defaultImage} alt={movie.title} /></div>
+            <div className={classes.overview}>{movie.overview}</div>
+        </div>
+        <div className={classes.details}>
+            <div>Release Date: {movie.release_date}</div>
+            <div>Vote: {movie.vote_average ? movie.vote_average.toFixed(1) : movie.vote_average} / 10 ({movie.vote_count})</div>
+            <div className={classes.genres}>{movie.genres?.map(x => <span key={x.id}>{x.name}</span>)}</div>
+        </div>
+        <a className={classes.source} href="https://www.themoviedb.org/" target='blank'>Data Source</a>
+    </div>;
 
     return (
-        <div>Details Page: {movieId} {console.log(movie)}</div>
-    );
+        <>
+            {isLoading ? <Loader /> : showMovie}
+        </>
+    )
 };
 
 export default MovieDetailsPage;
