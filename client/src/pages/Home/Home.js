d60c7aadc67d641f0261ec29ca0c1cd0926ac640
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-24 09:59:46 +0200

    Work on home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { getMoviesWithPages } from '../../services/moviesService';
+import { getSeriesWithPages } from '../../services/seriesService';
+import { getPeopleWithPages } from '../../services/peopleService';
+
+import Loader from '../../components/UI/Loader/Loader';
+
 const HomePage = () => {
+    const [movies, setMovies] = useState([]);
+    const [series, setSeries] = useState([]);
+    const [celebs, setCelebs] = useState([]);
+
+
     return (
-        <div>Home Page</div>
     );
 };
 
