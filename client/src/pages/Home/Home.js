afda18e1bd87c03c53c71d352094f6eee8e15150
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-11-24 10:00:46 +0200

    Show top 4 movies, celebs, series to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -12,9 +12,44 @@ const HomePage = () => {
     const [movies, setMovies] = useState([]);
     const [series, setSeries] = useState([]);
     const [celebs, setCelebs] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        const getMovies = async () => {
+            const movies = await getMoviesWithPages();
+            setMovies(movies.results.slice(0, 4));
+        };
+
+        const getSeries = async () => {
+            const series = await getSeriesWithPages();
+            setSeries(series.results.slice(0, 4));
+        };
+
+        const getCelebs = async () => {
+            const celebs = await getPeopleWithPages();
+            setCelebs(celebs.results.slice(0, 4));
+        };
+
+        setIsLoading(true);
+
+        const timer = setTimeout(async () => {
+            try {
+                Promise.all([getMovies(), getSeries(), getCelebs()]);
+            } catch (error) {
+                navigate('/error');
+            }
+
+            setIsLoading(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
 
     return (
+        <>
+            {isLoading ? <Loader /> : console.log(celebs)}
+        </>
     );
 };
 
