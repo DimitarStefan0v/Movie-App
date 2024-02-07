96fd8248678ce3591105dcfe3a9ae968bcf3abb4
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-07 11:04:38 +0200

    Removing simulated slowing when fetching data so we can show loader longer on Person details page

--- client/src/pages/People/PersonDetails.js
+++ client/src/pages/People/PersonDetails.js
@@ -10,50 +10,59 @@ import BackToTop from '../../components/UI/BackToTop/BackToTop';
 import classes from './PersonDetails.module.css';
 
 const PersonDetailsPage = () => {
-    const { personId } = useParams();
-    const [person, setPerson] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
+	const { personId } = useParams();
+	const [person, setPerson] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        const getPerson = async () => {
-            const person = await getPersonById(personId);
-            setPerson(person);
-        };
+	useEffect(() => {
+		setIsLoading(true);
+		getPersonById(personId)
+			.then((person) => {
+                setPerson(person);
+                setIsLoading(false);
+            })
+			.catch((err) => navigate('/error'));
 
-        setIsLoading(true);
+	}, [personId, navigate]);
 
-        const timer = setTimeout(async () => {
-            try {
-                await getPerson();
-            } catch (error) {
-                navigate('/error');
-            }
+	const showPerson = (
+		<div className={classes.wrapper}>
+			<h2>{person.name}</h2>
+			<div className={classes['image-overview']}>
+				<div className={classes['image-wrapper']}>
+					<img
+						src={
+							person.profile_path
+								? urls.images + person.profile_path
+								: urls.defaultImage
+						}
+						alt={person.name}
+					/>
+				</div>
+				<div className={classes.overview}>
+					{person.biography?.length > 700
+						? person.biography.substring(0, 700) + '...'
+						: person.biography}
+				</div>
+			</div>
+			<div className={classes.details}>
+				<div>
+					Born: {person.birthday}, {person.place_of_birth}
+				</div>
+			</div>
+			<a
+				className={classes.source}
+				href='https://www.themoviedb.org/'
+				target='blank'
+			>
+				Data Source
+			</a>
+			<BackToTop />
+		</div>
+	);
 
-            setIsLoading(false);
-        }, 1000);
-
-        return () => clearTimeout(timer);
-    }, [personId, navigate]);
-
-    const showPerson = <div className={classes.wrapper}>
-        <h2>{person.name}</h2>
-        <div className={classes['image-overview']}>
-            <div className={classes['image-wrapper']}><img src={person.profile_path ? urls.images + person.profile_path : urls.defaultImage} alt={person.name} /></div>
-            <div className={classes.overview}>{person.biography?.length > 700 ? person.biography.substring(0, 700) + '...' : person.biography}</div>
-        </div>
-        <div className={classes.details}>
-            <div>Born: {person.birthday}, {person.place_of_birth}</div>
-        </div>
-        <a className={classes.source} href="https://www.themoviedb.org/" target='blank'>Data Source</a>
-        <BackToTop />
-    </div>;
-
-    return (
-        <>
-            {isLoading ? <Loader /> : showPerson}
-        </>
-    );
+	return <>{isLoading ? <Loader /> : showPerson}</>;
 };
 
 export default PersonDetailsPage;
