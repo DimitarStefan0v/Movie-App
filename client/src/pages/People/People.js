0fe22e2154852ba62bac9bc446890992a1481029
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-07 11:04:28 +0200

    Removing simulated slowing when fetching data so we can show loader longer on People page

--- client/src/pages/People/People.js
+++ client/src/pages/People/People.js
@@ -10,49 +10,46 @@ import Loader from '../../components/UI/Loader/Loader';
 import BackToTop from '../../components/UI/BackToTop/BackToTop';
 
 const PeoplePage = () => {
-    const [peopleData, setPeopleData] = useState({});
-    const [currentPage, setCurrentPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
+	const [peopleData, setPeopleData] = useState({});
+	const [currentPage, setCurrentPage] = useState(1);
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchPeople = async () => {
-            const people = await getPeopleWithPages(currentPage);
-            setPeopleData(people);
-            window.scrollTo(0, 0);
-        }
+	useEffect(() => {
+		setIsLoading(true);
+		getPeopleWithPages(currentPage)
+			.then((people) => {
+                setPeopleData(people);
+                window.scrollTo(0, 0);
+                setIsLoading(false);
+            })
+			.catch((err) => navigate('/error'));
 
-        setIsLoading(true);
+	}, [currentPage, navigate]);
 
-        const timer = setTimeout(async () => {
-            try {
-                await fetchPeople();
-            } catch (error) {
-                navigate('/error')
-            }
+	const changePageHandler = (selectedPage) => {
+		setCurrentPage(selectedPage);
+	};
 
-            setIsLoading(false);
-        }, 1000);
+	const showPeople = (
+		<>
+			<CardList>
+				{peopleData.results?.map((person) => (
+					<Link key={person.id} to={`/person/${person.id}/details`}>
+						<Card poster={person.profile_path} title={person.name} />
+					</Link>
+				))}
+			</CardList>
+			<Pagination
+				changePageHandler={changePageHandler}
+				currentPage={currentPage}
+				lastPage={peopleData.total_pages > 500 ? 500 : peopleData.total_pages}
+			/>
+			<BackToTop />
+		</>
+	);
 
-        return () => clearTimeout(timer);
-    },
-        [currentPage, navigate]);
-
-    const changePageHandler = (selectedPage) => {
-        setCurrentPage(selectedPage);
-    };
-
-    const showPeople = <>
-        <CardList>{peopleData.results?.map(person => <Link key={person.id} to={`/person/${person.id}/details`}><Card poster={person.profile_path} title={person.name} /></Link>)}</CardList>
-        <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={peopleData.total_pages > 500 ? 500 : peopleData.total_pages} />
-        <BackToTop />
-    </>;
-
-    return (
-        <>  
-            {isLoading ? <Loader /> : showPeople}
-        </>
-    );
+	return <>{isLoading ? <Loader /> : showPeople}</>;
 };
 
 export default PeoplePage;
