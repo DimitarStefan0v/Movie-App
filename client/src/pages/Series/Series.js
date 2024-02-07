0e424002165702da454d8200e6c572b38009e723
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-07 11:05:14 +0200

    Removing simulated slowing when fetching data so we can show loader longer on Series page

--- client/src/pages/Series/Series.js
+++ client/src/pages/Series/Series.js
@@ -11,55 +11,55 @@ import Loader from '../../components/UI/Loader/Loader';
 import BackToTop from '../../components/UI/BackToTop/BackToTop';
 
 const SeriesPage = () => {
-    const [seriesData, setSeriesData] = useState({});
-    const [currentPage, setCurrentPage] = useState(1);
-    const [currentFilter, setCurrentFilter] = useState('popularity.desc');
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
+	const [seriesData, setSeriesData] = useState({});
+	const [currentPage, setCurrentPage] = useState(1);
+	const [currentFilter, setCurrentFilter] = useState('popularity.desc');
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchSeries = async () => {
-            const series = await getSeriesWithPages(currentPage, currentFilter);
-            setSeriesData(series);
-            window.scrollTo(0, 0);
-        };
+	useEffect(() => {
+		setIsLoading(true);
+		getSeriesWithPages(currentPage, currentFilter)
+			.then((series) => {
+                setSeriesData(series);
+                window.scrollTo(0, 0);
+                setIsLoading(false);
+            })
+			.catch((err) => navigate('/error'));
 
-        setIsLoading(true);
+	}, [currentPage, currentFilter, navigate]);
 
-        const timer = setTimeout(async () => {
-            try {
-                await fetchSeries();
-            } catch (error) {
-                navigate('/error')
-            }
+	const changePageHandler = (selectedPage) => {
+		setCurrentPage(selectedPage);
+	};
 
-            setIsLoading(false);
-        }, 1000);
+	const changeSortQueryHandler = (selectedQuery) => {
+		setCurrentFilter(selectedQuery);
+	};
 
-        return () => clearTimeout(timer);
-    },
-        [currentPage, currentFilter, navigate]);
+	const showSeries = (
+		<>
+			<Sorter
+				currentFilter={currentFilter}
+				changeSortQueryHandler={changeSortQueryHandler}
+			/>
+			<CardList>
+				{seriesData.results?.map((serie) => (
+					<Link key={serie.id} to={`/tv/${serie.id}/details`}>
+						<Card poster={serie.poster_path} title={serie.name} />
+					</Link>
+				))}
+			</CardList>
+			<Pagination
+				changePageHandler={changePageHandler}
+				currentPage={currentPage}
+				lastPage={seriesData.total_pages > 500 ? 500 : seriesData.total_pages}
+			/>
+			<BackToTop />
+		</>
+	);
 
-    const changePageHandler = (selectedPage) => {
-        setCurrentPage(selectedPage);
-    };
-
-    const changeSortQueryHandler = (selectedQuery) => {
-        setCurrentFilter(selectedQuery);
-    };
-
-    const showSeries = <>
-        <Sorter currentFilter={currentFilter} changeSortQueryHandler={changeSortQueryHandler} />
-        <CardList>{seriesData.results?.map(serie => <Link key={serie.id} to={`/tv/${serie.id}/details`}><Card poster={serie.poster_path} title={serie.name} /></Link>)}</CardList>
-        <Pagination changePageHandler={changePageHandler} currentPage={currentPage} lastPage={seriesData.total_pages > 500 ? 500 : seriesData.total_pages} />
-        <BackToTop />
-    </>;
-
-    return (
-        <>
-            {isLoading ? <Loader /> : showSeries}
-        </>
-    );
+	return <>{isLoading ? <Loader /> : showSeries}</>;
 };
 
 export default SeriesPage;
